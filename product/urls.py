283f4832e78e0867d8a3a94233b5b0cc629bd2a1
parsa <mpmb81@gmail.com>
2024-12-29 18:37:36 +0330

    Add image API: Upload, Download, Delete

--- product/urls.py
+++ product/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import ProductCreateView, ProductListView, ProductDetailsView
+from .views import ProductCreateView, ProductListView, ImageUploadView, ImageDeleteView, ImageDownloadView, ProductDetailsView
 
 urlpatterns = [
     path('product/', ProductCreateView.as_view(), name='product-create'),
     path('product/<int:product_id>/', ProductDetailsView.as_view(), name='product-get'),
     path('products/', ProductListView.as_view(), name='product-list'),
-]
+    path('image/upload/', ImageUploadView.as_view(), name='image-upload'),
+    path('image/<int:imageId>/', ImageDeleteView.as_view(), name='image-delete'),
+    path('image/<int:imageId>/download/', ImageDownloadView.as_view(), name='image-download'),
+] 
