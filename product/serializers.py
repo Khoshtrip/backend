bad5fdfee88e215d72a8034dd6af17e74bc4372d
HamedSY <hamed007.saboor@gmail.com>
2024-12-31 19:16:00 +0330

    fix: image serializing

--- product/serializers.py
+++ product/serializers.py
@@ -1,4 +1,3 @@
-from rest_framework import serializers
 from .models import Product
 from rest_framework import serializers
 from .models import Image
@@ -9,15 +8,19 @@ class ImageSerializer(serializers.ModelSerializer):
         fields = ['id', 'file', 'uploaded_at']
 
 class ProductSerializer(serializers.ModelSerializer):
-    image = serializers.SerializerMethodField()
+    images = serializers.ListField(
+        child=serializers.IntegerField(),
+        allow_empty=True,
+        default=list
+    )
 
     class Meta:
         model = Product
         fields = [
             'id', 'name', 'summary', 'description', 'price', 
-            'discount', 'stock', 'category', 'image', 'isActive', 
+            'discount', 'stock', 'category', 'images', 'isActive',
             'created_at', 'updated_at'
         ]
 
-    def get_image(self, obj):
-        return obj.images[0] if obj.images else None 
+    # def get_images(self, obj):
+    #     return obj.images[0] if obj.images else None
